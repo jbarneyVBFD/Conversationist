bf23d44a591b0f5a4802b929e1c2ceda44f4b990
jbarneyVBFD <john.barney.2009@gmail.com>
2024-06-07 19:44:24 -0400

    Initial commit

---
+++ Conversationist/ConversationistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversationistApp.swift
+//  Conversationist
+//
+//  Created by John Barney on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ConversationistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
