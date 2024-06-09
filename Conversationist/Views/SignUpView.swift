280d838ee05a7c67635512bc7d69f260a88038a9
jbarneyVBFD <john.barney.2009@gmail.com>
2024-06-09 16:29:47 -0400

    Created signup func

--- Conversationist/Views/SignUpView.swift
+++ Conversationist/Views/SignUpView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AWSCognitoIdentityProvider
 
 struct SignUpView: View {
     @State private var password: String = ""
@@ -16,6 +17,34 @@ struct SignUpView: View {
             TextField("Email", text: $email)
                 .textFieldStyle(.roundedBorder)
                 .padding()
+            TextField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button("Sign Up") {
+                signUp()
+            }
+        }
+    }
+    
+    func signUp() {
+        guard let pool = AWSCognitoIdentityUserPool(forKey: "UserPool") else {
+            message = "User pool not configured."
+            return
+        }
+            
+        let userAttributes = [
+            AWSCognitoIdentityUserAttributeType(name: "email", value: email)
+        ]
+            
+        pool.signUp(email, password: password, userAttributes: userAttributes, validationData: nil).continueWith { task in
+            DispatchQueue.main.async {
+                if let error = task.error as NSError? {
+                    message = "Error: \(error.userInfo["message"] as? String ?? "Unknown error")"
+                } else {
+                    message = "Sign-up successful"
+                }
+            }
+            return nil
         }
     }
 }
